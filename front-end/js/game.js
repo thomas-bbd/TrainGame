f4090bf393c88e15d21b242653fcf0e5ed465e8b
Janco Venter <jan@bbd.co.za>
2023-07-26 14:59:07 +0200

    Slight game logic changes and other changes

--- front-end/js/game.js
+++ front-end/js/game.js
@@ -1,87 +1,26 @@
 const questions = [
   {
-    question: "What is the capital city of South Africa?",
-    optionA: "Cape Town",
-    optionB: "Johannesburg",
-    optionC: "Pretoria",
-    optionD: "Durban",
-    correctOption: "optionC",
-  },
-  {
-    question:
-      "Which famous South African leader was imprisoned on Robben Island?",
-    optionA: "Nelson Mandela",
-    optionB: "Desmond Tutu",
-    optionC: "F.W. de Klerk",
-    optionD: "Thabo Mbeki",
-    correctOption: "optionA",
-  },
-  {
-    question: "Which is the largest national park in South Africa?",
-    optionA: "Kruger National Park",
-    optionB: "Table Mountain National Park",
-    optionC: "Addo Elephant National Park",
-    optionD: "Golden Gate Highlands National Park",
-    correctOption: "optionA",
-  },
-  {
-    question: "In which province is the famous Garden Route located?",
-    optionA: "KwaZulu-Natal",
-    optionB: "Western Cape",
-    optionC: "Eastern Cape",
-    optionD: "Limpopo",
+    question: "Is bread heavier than a train?",
+    optionA: "True",
+    optionB: "False",
     correctOption: "optionB",
   },
   {
-    question: "What is the currency of South Africa?",
-    optionA: "Rand",
-    optionB: "Pula",
-    optionC: "Dollar",
-    optionD: "Euro",
+    question: "Is the moon heavier than a train?",
+      optionA: "True",
+      optionB: "False",
+      correctOption: "optionA",
+  },
+  {
+    question: "Is the earth heavier than a train?",
+    optionA: "True",
+    optionB: "False",
     correctOption: "optionA",
   },
   {
-    question:
-      "Which iconic mountain is a major tourist attraction in Cape Town?",
-    optionA: "Mount Kilimanjaro",
-    optionB: "Mount Elgon",
-    optionC: "Table Mountain",
-    optionD: "Mount Kenya",
-    correctOption: "optionC",
-  },
-  {
-    question: "What is the largest city in South Africa?",
-    optionA: "Durban",
-    optionB: "Pretoria",
-    optionC: "Johannesburg",
-    optionD: "Cape Town",
-    correctOption: "optionC",
-  },
-  {
-    question:
-      "Which famous South African anti-apartheid activist and politician became the country's president after apartheid?",
-    optionA: "Desmond Tutu",
-    optionB: "F.W. de Klerk",
-    optionC: "Thabo Mbeki",
-    optionD: "Nelson Mandela",
-    correctOption: "optionD",
-  },
-  {
-    question:
-      "What is the name of the famous South African prison where Nelson Mandela was held for 18 years?",
-    optionA: "Pollsmoor Prison",
-    optionB: "Robben Island",
-    optionC: "Sun City Prison",
-    optionD: "Victor Verster Prison",
-    correctOption: "optionB",
-  },
-  {
-    question:
-      "Which sport is widely popular in South Africa and is known as the 'Rainbow Nation's pride'?",
-    optionA: "Football (Soccer)",
-    optionB: "Rugby",
-    optionC: "Cricket",
-    optionD: "Golf",
+    question: "Is a toaster heavier than a train?",
+    optionA: "True",
+    optionB: "False",
     correctOption: "optionB",
   },
 ];
@@ -89,7 +28,7 @@ const questions = [
 let shuffledQuestions = [];
 
 function handleQuestions() {
-  while (shuffledQuestions.length <= 9) {
+  while (shuffledQuestions.length <= 3) {
     const random = questions[Math.floor(Math.random() * questions.length)];
     if (!shuffledQuestions.includes(random)) {
       shuffledQuestions.push(random);
@@ -105,18 +44,10 @@ let indexNumber = 0;
 function NextQuestion(index) {
   handleQuestions();
   const currentQuestion = shuffledQuestions[index];
-  document.getElementById("question-number").innerHTML = questionNumber;
-  document.getElementById("player-score").innerHTML = playerScore;
-  document.getElementById("display-question").innerHTML =
-    currentQuestion.question;
-  document.getElementById("option-one-label").innerHTML =
-    currentQuestion.optionA;
-  document.getElementById("option-two-label").innerHTML =
-    currentQuestion.optionB;
-  document.getElementById("option-three-label").innerHTML =
-    currentQuestion.optionC;
-  document.getElementById("option-four-label").innerHTML =
-    currentQuestion.optionD;
+  document.getElementById("player-score").innerText = playerScore;
+  document.getElementById("display-question").innerText = currentQuestion.question;
+  document.getElementById("option-one-label").innerText = currentQuestion.optionA;
+  document.getElementById("option-two-label").innerText = currentQuestion.optionB;
 }
 
 function checkForAnswer() {
@@ -133,9 +64,7 @@ function checkForAnswer() {
 
   if (
     options[0].checked === false &&
-    options[1].checked === false &&
-    options[2].checked === false &&
-    options[3].checked == false
+    options[1].checked === false
   ) {
     document.getElementById("option-modal").style.display = "flex";
   }
@@ -152,11 +81,10 @@ function checkForAnswer() {
       const wrongLabelId = option.labels[0].id;
       document.getElementById(wrongLabelId).style.backgroundColor = "red";
       document.getElementById(correctOption).style.backgroundColor = "green";
-      wrongAttempt++;
-      indexNumber++;
       setTimeout(() => {
         questionNumber++;
       }, 1000);
+      handleEndGame()
     }
   });
 }
@@ -165,7 +93,7 @@ function handleNextQuestion() {
   checkForAnswer();
   unCheckRadioButtons();
   setTimeout(() => {
-    if (indexNumber <= 9) {
+    if (indexNumber <= 3) {
       NextQuestion(indexNumber);
     } else {
       handleEndGame();
@@ -189,26 +117,7 @@ function unCheckRadioButtons() {
 }
 
 function handleEndGame() {
-  let remark = null;
-  let remarkColor = null;
-
-  if (playerScore <= 3) {
-    remark = "Bad Grades, Keep Practicing.";
-    remarkColor = "red";
-  } else if (playerScore >= 4 && playerScore < 7) {
-    remark = "Average Grades, You can do better.";
-    remarkColor = "orange";
-  } else if (playerScore >= 7) {
-    remark = "Excellent, Keep the good work going.";
-    remarkColor = "green";
-  }
-  const playerGrade = (playerScore / 10) * 100;
-
-  document.getElementById("remarks").innerHTML = remark;
-  document.getElementById("remarks").style.color = remarkColor;
-  document.getElementById("grade-percentage").innerHTML = playerGrade;
-  document.getElementById("wrong-answers").innerHTML = wrongAttempt;
-  document.getElementById("right-answers").innerHTML = playerScore;
+  document.getElementById("right-answers").innerText = playerScore;
   document.getElementById("score-modal").style.display = "flex";
 }
 
