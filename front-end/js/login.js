4acd9b0b04463e68d1b1a652fe0b27e8805d4b4a
Janco Venter <jan@bbd.co.za>
2023-07-27 20:41:42 +0200

    Register page working

--- front-end/js/login.js
+++ front-end/js/login.js
@@ -4,39 +4,46 @@ const poolData = {
 };
 const userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
 function loginUser(event) {
-event.preventDefault();
+    event.preventDefault();
 
-const username = document.getElementById('username').value;
-const password = document.getElementById('password').value;
-const inlineError = document.getElementById('error-inline');
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    const inlineError = document.getElementById('error-inline');
 
-const authenticationData = {
-    Username: username,
-    Password: password,
-};
+    const authenticationData = {
+        Username: username,
+        Password: password,
+    };
 
-const authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
-const userData = {
-    Username: username,
-    Pool: userPool,
-};
+    const authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
+    const userData = {
+        Username: username,
+        Pool: userPool,
+    };
 
-const cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+    const cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
 
-cognitoUser.authenticateUser(authenticationDetails, {
-    onSuccess: function (session) {
-    localStorage.setItem('jwt',session.getAccessToken().getJwtToken())
-    window.location.href = "/front-end/home.html"
-},
-    onFailure: function (err) {
-    inlineError.innerText=err.message;
-    console.error('Error logging in:', err.message || JSON.stringify(err));
+    cognitoUser.authenticateUser(authenticationDetails, {
+        onSuccess: function (session) {
+        localStorage.setItem('jwt',session.getAccessToken().getJwtToken())
+        window.location.href = "./front-end/home.html"
     },
-});
+        onFailure: function (err) {
+        inlineError.innerText=err.message;
+        console.error('Error logging in:', err.message || JSON.stringify(err));
+        },
+    });
 }
 
 function Register(event) {
     event.preventDefault();
 
+    window.location.href = "./front-end/register.html"
+}
+
+function Login(event) {
+    event.preventDefault();
+
+    
     window.location.href = "./front-end/register.html"
 }
