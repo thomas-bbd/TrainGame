1c3512f3e235a2fb52f06f8310220d045b9dcf98
Thomas Stevens <thomas.stevens@vcontractor.co.za>
2023-07-28 01:11:23 +0200

    Updated register screen

--- front-end/js/register.js
+++ front-end/js/register.js
@@ -1,3 +1,19 @@
+const loginRedirectBtn = document.getElementById("login-redirect");
+const registerSection = document.getElementById("register-section");
+const authSection = document.getElementById("auth-section");
+const mainAside = document.getElementById("main-aside");
+const overlay = document.getElementById('popupOverlay');
+const inlineError = document.getElementById('error-inline');
+
+
+const baseURI = window.location.origin;
+
+loginRedirectBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.location.replace(baseURI)
+});
+
+
 const poolData = {
     UserPoolId: 'us-east-1_6iVfhJwtH',      // Replace with your actual User Pool ID
     ClientId: 'l0nhuftvutntsqncbcraodmct',       // Replace with your actual App Client ID
@@ -5,10 +21,8 @@ const poolData = {
 
 const userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
 
-function validatePassword(){
-    const password = document.getElementById('password').value;
+function validatePassword(password){
     const passwordRegex = new RegExp(/(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/);
-    const inlineError = document.getElementById('error-inline');
     if(!passwordRegex.test(password)){
         inlineError.innerText = "Invalid password, you need 1 uppercase, 1 lowercase, 1 special character, a number and a minimum of 8 characters."
         setTimeout(() => {inlineError.innerText = ''}, 10000)
@@ -18,47 +32,36 @@ function validatePassword(){
 };
 
 function registerUser(event) {
-    event.preventDefault();
-    // togglePopup()
-
+    event.preventDefault();    
     try{
-
         const username = document.getElementById('username').value;
         const password = document.getElementById('password').value;
         const email = document.getElementById('email').value;
-        // Add other form field values as needed
         
-        if(!validatePassword()){
+        if(!validatePassword(password)){
             return;
         }
 
         const attributeList = [
             new AmazonCognitoIdentity.CognitoUserAttribute({ Name: 'email', Value: email }),
             new AmazonCognitoIdentity.CognitoUserAttribute({ Name: 'preferred_username', Value: username }),
-            // Add other attributes as needed
         ];
         userPool.signUp(username, password, attributeList, null, function (err, result) {
         if (err) {
             console.error('Error registering user:', err.message || JSON.stringify(err));
+            inlineError.innerText = err.message || JSON.stringify(err);
             return;
         }else{
-            document.getElementById('registerUserForm').style.display = 'none';
-            document.getElementById('confirmRegister').style.display = 'flex';
+            authSection.style.display = "block";
+            registerSection.style.display = "none";
         }
-        // You can redirect the user to a confirmation page or perform other actions here.
         });
-    }catch(e){
+    } catch(e) {
         console.log(e)
     }
 }
 
-function togglePopup() {
-    var overlay = document.getElementById('popupOverlay');
-    overlay.style.display = overlay.style.display === 'block' ? 'none' : 'block';
-}
-
 function authorizeUser(event){
-
     event.preventDefault();
     const username = document.getElementById('username').value;
     const authCode = document.getElementById('auth-code').value;
@@ -73,14 +76,10 @@ function authorizeUser(event){
         if (err) {
           console.log('Confirmation error:', err.message || JSON.stringify(err));
         } else {
-            togglePopup()
-            setTimeout(()=> {window.location.replace('https://dnhcmoxb4x8e8.cloudfront.net/')}, 5000);
-        //   go to login
+            mainAside.style.display = "none";
+            overlay.style.display = "block";
+            setTimeout(()=> {window.location.replace(baseURI)}, 5000);
         }
     });
 
-}
-
-function navToLogin(){
-    window.location.href = "../.../index.html"
 }
